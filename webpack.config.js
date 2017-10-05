bf9d16aea09f75d407da4d6348a73280f3a18165
Emran Hossen <emran04ch@gmail.com>
2017-10-05 09:26:07 +0600

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const config = {
+  context: path.resolve(__dirname, 'src'),  
+  entry: {
+    // removing 'src' directory from entry point, since 'context' is taking care of that
+    app: './app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './assets/js/[name].bundle.js'
+  }
+}
+
+module.exports = config;
