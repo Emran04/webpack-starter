a8d660b406198dc774f94b11e0eaeb0d0bd2a7db
Emran Hossen <emran04ch@gmail.com>
2017-10-06 16:35:54 +0600

    plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,48 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const config = {
-  context: path.resolve(__dirname, 'src'),  
+  context: path.resolve(__dirname, 'src'),
+
   entry: {
     // removing 'src' directory from entry point, since 'context' is taking care of that
     app: './app.js'
   },
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: './assets/js/[name].bundle.js'
-  }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: /src/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist'])
+  ],
+
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist/assets/media"),
+    compress: true,
+    port: 2000,
+    stats: 'errors-only',
+    open: true
+  },
+
+  devtool: 'inline-source-map'
 }
 
 module.exports = config;
